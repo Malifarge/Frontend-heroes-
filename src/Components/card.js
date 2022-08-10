3ba2812e9e0c7ba395061cae2c70e511710eb86e
Thomas <malifarge.thomas9@gmail.com>
2022-08-10 21:54:58 +0200

    style for Alive and color

--- src/Components/card.js
+++ src/Components/card.js
@@ -2,14 +2,17 @@ import '../styles/card.css'
 
 const Card = (props) =>{
 
-    const {name,power,age,image} = props.heroe
+    const {name,power,age,image,color,isAlive} = props.heroe
+    console.log(color);
 
     return (
-        <div>
+        <div className='border' style={isAlive ? {borderColor: "black" }: {borderColor: "red"}}>
             <div className='heroeImg' style={{
                 backgroundImage: `url(${image})`
             }}></div>
-            <h2>{name}</h2>
+            <h2 style={{
+                textDecorationColor: `${color}`
+            }}>{name}</h2>
             <p>{age} ans</p>
 
             <h2>Power</h2>
