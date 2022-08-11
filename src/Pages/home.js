34effd51221f3a5611b9d8637a6bb4fb08f570bf
Thomas <malifarge.thomas9@gmail.com>
2022-08-11 13:17:54 +0200

    form new hero re-style

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -107,40 +107,40 @@ const Home = () =>{
 
                 <h2>New hero</h2>
 
-                <form className="flex clmn g-30" onSubmit={handleSubmit}>
+                <form className="flex clmn g-30 center" onSubmit={handleSubmit}>
 
-                    <div className="flex g-30">
+                    <div className="flex s-b g-30">
                         <label htmlFor="slug">slug</label>
                         <input type="text" onChange={handlechangeSlug} name="slug" />
                     </div>
 
-                    <div className="flex g-30">
+                    <div className="flex s-b g-30">
                         <label htmlFor="name">name</label>
                         <input type="text" onChange={handlechangeName} name="name" />
                     </div>
 
-                    <div className="flex g-30">
+                    <div className="flex s-b g-30">
                         <label htmlFor="power">power</label>
                         <input type="text" value={powerNew} onChange={handlechangePower} name="power" />
                         <button onClick={handleArrayPowerClick} type="button">+</button>
                     </div>
 
-                    <div className="flex g-30">
+                    <div className="flex s-b g-30">
                         <label htmlFor="color">color</label>
                         <input type="text" onChange={handlechangeColor} name="color" />
                     </div>
 
-                    <div className="flex g-30">
+                    <div className="flex s-b g-30">
                         <label htmlFor="isAlive">isAlive</label>
                         <input type="checkbox" name="isAlive" onChange={handleCheck} />
                     </div>
 
-                    <div className="flex g-30">
+                    <div className="flex s-b g-30">
                         <label htmlFor="age">age</label>
                         <input type="text" onChange={handlechangeAge} name="age" />
                     </div>
 
-                    <div className="flex g-30">
+                    <div className="flex s-b g-30">
                         <label htmlFor="image">image</label>
                         <input type="text" onChange={handlechangeImage} name="image" />
                     </div>
